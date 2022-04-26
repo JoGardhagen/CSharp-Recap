fec7c7bf4e5b65d246a1cd32394bb6994b3bbee1
JoGardhagen <92914305+JoGardhagen@users.noreply.github.com>
2022-04-26 23:47:01 +0200

    Add files via upload

---
+++ inputSwap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace inputSwap
+{   
+    class inputSwap{
+        static public void Main(string[]args){
+            System.Console.WriteLine("Skriv in något !");
+            string input = Console.ReadLine();
+            System.Console.WriteLine(swapString(input));
+
+        }
+        static string swapString(String input){
+            
+            char[] charA= input.ToCharArray(); 
+            Array.Reverse(charA);
+            return  new string(charA);
+
+        }
+    }
+    
+}
